ccf547126dedcbe88a8cb477a7e38e3ff6a5e040
DavidLucha <davidalucha@gmail.com>
2022-08-03 07:10:29 +1000

    fixed stage 1 loading

--- bash_scripts/RUN_Pretrain_Stage1.sh
+++ bash_scripts/RUN_Pretrain_Stage1.sh
@@ -24,10 +24,10 @@ RUN_NAME=$(date +%Y%m%d-%H%M%S)
 
 # Study 1
 # Pretrain one network. Used in both GOD and NSD.
-echo "Running pretrain at $(date +%Y%m%d-%H%M%S)"
-srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/pretrain.py --run_name RUN2_1_pretrain_$RUN_NAME --lr 0.0001 --gamma 1.0 --equilibrium_game y --backprop_method clip --num_workers 2 --epochs 100 --dataset both --loss_method Maria --optim_method RMS --seed 277603 --message "Proper run, using pretrain on 30k into stage 1 with only NSD. Pretrain."
-echo "Pretrain complete at $(date +%Y%m%d-%H%M%S)"
+# echo "Running pretrain at $(date +%Y%m%d-%H%M%S)"
+# srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/pretrain.py --run_name RUN2_1_pretrain_$RUN_NAME --lr 0.0001 --gamma 1.0 --equilibrium_game y --backprop_method clip --num_workers 2 --epochs 100 --dataset both --loss_method Maria --optim_method RMS --seed 277603 --message "Proper run, using pretrain on 30k into stage 1 with only NSD. Pretrain."
+# echo "Pretrain complete at $(date +%Y%m%d-%H%M%S)"
 
 echo "Running stage 1 at $(date +%Y%m%d-%H%M%S)"
-srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage1.py --run_name RUN2_1_stage1_$RUN_NAME --pretrained_net RUN2_1_pretrain_$RUN_NAME --load_epoch 99 --lr 0.0001 --gamma 1.0 --equilibrium_game y --backprop_method clip --num_workers 2 --epochs 200 --dataset NSD --loss_method Maria --optim_method RMS --seed 277603 --message "Proper run, using pretrain on 30k into stage 1 with only NSD. Stage 1."
+srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage1.py --run_name RUN2_1_stage1_$RUN_NAME --pretrained_net RUN2_1_pretrain_20220802-225243  --load_epoch 99 --lr 0.0001 --backprop_method clip --num_workers 2 --epochs 200 --dataset NSD --seed 277603 --message "Proper run, using pretrain on 30k into stage 1 with only NSD. Stage 1."
 echo "Stage 1 complete at $(date +%Y%m%d-%H%M%S)"
