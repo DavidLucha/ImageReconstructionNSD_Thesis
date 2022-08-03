76279e6c9d11c6ce544fa57757e526486ed79dc5
DavidLucha <davidalucha@gmail.com>
2022-08-03 21:36:46 +1000

    stage 3 and batch scriping test

--- bash_scripts/RUN_Stage2_Stage3_Iter_Test.sh
+++ bash_scripts/RUN_Stage2_Stage3_Iter_Test.sh
@@ -31,12 +31,12 @@ PRE_NET="RUN1_1_stage1_only_20220802-225250"
 # Study 1
 # Run stage 2
 echo "Running stage 2 at $(date +%Y%m%d-%H%M%S)"
-srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage2_iter.py --run_name iterRUN2_1.8max_stage2_subj0${SUBJ}_${RUN_NAME} --vox_res ${VOX_RES} --set_size ${SET_SIZE} --subject ${SUBJ} --pretrained_net ${PRE_NET} --load_from ${LOAD_FROM} --load_epoch ${LOAD_EP} --lr 0.0001 --backprop_method clip --num_workers 2 --epochs 150 --iters 400 --dataset NSD --seed 277603 --message "Stage 2, subject ${SUBJ}, at ${VOX_RES} voxel resolution, with set size of: ${SET_SIZE}, higher lr (maria)"
+srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage2_iter.py --run_name iterRUN2_1.8max_stage2_subj0${SUBJ}_${RUN_NAME} --vox_res ${VOX_RES} --set_size ${SET_SIZE} --subject ${SUBJ} --pretrained_net ${PRE_NET} --load_from ${LOAD_FROM} --load_epoch ${LOAD_EP} --lr 0.0001 --backprop_method clip --num_workers 2 --epochs 150 --iters 30000 --dataset NSD --seed 277603 --message "Stage 2, subject ${SUBJ}, at ${VOX_RES} voxel resolution, with set size of: ${SET_SIZE}, higher lr (maria)"
 echo "Stage 2 complete at $(date +%Y%m%d-%H%M%S)"
 
 # Run stage 3
 echo "Running stage 3 at $(date +%Y%m%d-%H%M%S)"
-srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage3_iter.py --run_name RUN_1.8max_stage3_subj0${SUBJ}_${RUN_NAME} --vox_res ${VOX_RES} --set_size ${SET_SIZE} --subject ${SUBJ} --stage_2_trained iterRUN2_1.8max_stage2_subj0${SUBJ}_${RUN_NAME} --load_epoch final --lr 0.0001 --backprop_method clip --num_workers 2 --epochs 150 --iters 400 --dataset NSD --seed 277603 --message "Stage 3, subject ${SUBJ}, at ${VOX_RES} voxel resolution, with set size of: ${SET_SIZE}"
+srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage3_iter.py --run_name RUN_1.8max_stage3_subj0${SUBJ}_${RUN_NAME} --vox_res ${VOX_RES} --set_size ${SET_SIZE} --subject ${SUBJ} --stage_2_trained iterRUN2_1.8max_stage2_subj0${SUBJ}_${RUN_NAME} --load_epoch final --lr 0.0001 --backprop_method clip --num_workers 2 --epochs 150 --iters 15000 --dataset NSD --seed 277603 --message "Stage 3, subject ${SUBJ}, at ${VOX_RES} voxel resolution, with set size of: ${SET_SIZE}"
 echo "Stage 3 complete at $(date +%Y%m%d-%H%M%S)"
 
 # tar the folder after complete
