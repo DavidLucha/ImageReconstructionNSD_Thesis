55f47483eb5ec45c5b204205d6c29975b16b9ff0
DavidLucha <davidalucha@gmail.com>
2022-08-11 02:49:52 +1000

    updates to stage 3 WAE

--- bash_scripts/WAE_Stage3_David_1.sh
+++ bash_scripts/WAE_Stage3_David_1.sh
@@ -24,6 +24,6 @@ RUN_NAME=$(date +%Y%m%d-%H%M%S)
 
 # Study 1
 # Pretrain one network. Used in both GOD and NSD.
-echo "Running pretrain at $(date +%Y%m%d-%H%M%S)"
-srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage3_WAE_iter.py --run_name WAE_Stage3_David_1_Test_$RUN_NAME --load_from pretrain --st1_net WAE_Maria_512_New_Test_20220811-003030 --st1_load_epoch 20 --st2_net WAE_Stage2_David_1_Test_$RUN_NAME --st2_load_epoch final --disc_loss David --WAE_loss Both --lambda_WAE 1 --batch_size 64 --lr_dec 0.00003 --lr_disc 0.00001 --valid_shuffle True --latent_dims 512 --vox_res 1.8mm --set_size max --subject 1 --ROI VC --num_workers 2 --epochs 110 --iters 30000 --dataset NSD --seed 277603 --message "First double test using ideal david loss. Loading discrim from stage 1 and push cog latent to samp"
-echo "Pretrain complete at $(date +%Y%m%d-%H%M%S)"
+echo "Running stage 3 at $(date +%Y%m%d-%H%M%S)"
+srun -N 1 -p gpu --gres=gpu:1 --mpi=pmi2 python /clusterdata/uqdlucha/scripts/deepReconPyTorch/train_stage3_WAE_iter.py --run_name WAE_Stage3_David_1_Test_$RUN_NAME --load_from pretrain --st1_net WAE_Maria_512_New_Test_20220811-003030 --st1_load_epoch 10 --st2_net WAE_Stage2_David_1_Test_20220811-015952 --st2_load_epoch 9 --disc_loss David --WAE_loss Both --lambda_WAE 1 --batch_size 64 --lr_dec 0.00003 --lr_disc 0.00001 --valid_shuffle True --latent_dims 512 --vox_res 1.8mm --set_size max --subject 1 --ROI VC --num_workers 2 --epochs 10 --iters 10000 --dataset NSD --seed 277603 --message "First double test using ideal david loss. Loading discrim from stage 1 and push cog latent to samp"
+echo "Stage 3 complete at $(date +%Y%m%d-%H%M%S)"
